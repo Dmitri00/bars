91a5a0a3f165a5a58b1e1e9c0862a461da83f24e
Dmitri00 <dima.toichkin@gmail.com>
2018-11-29 11:14:01 +0300

    Added comments on bugs

--- dev/soft/bars_stm_test/main.c
+++ dev/soft/bars_stm_test/main.c
@@ -3,19 +3,23 @@
 #include "board.h"
 #include "tools.h"
 #define RTCWUT_FREQ 32 /* Hz  (0.03125 ms) */
-// 
+/* Problems:
+ * 1) COUNT0,COUNT1 don't work together with BUTTONS
+ * 2) PC[5] always produces interrupts
+ * 3) BUTTON's interrupts aren't being set up by GPIO
+ */
 void RTC_init();
 int main( void )
 {
     
-    // Both counters work properly, when
-    GPIO_Init(COUNT0_GPIO_PORT,COUNT0_GPIO_PIN, GPIO_Mode_In_FL_IT);
+    // Both counters work properly
+    //GPIO_Init(COUNT0_GPIO_PORT,COUNT0_GPIO_PIN, GPIO_Mode_In_FL_IT);
     //GPIO_Init(COUNT1_GPIO_PORT,COUNT1_GPIO_PIN, GPIO_Mode_In_FL_IT);
     //Set exti pin sensitivity
 
+    //Buttons work, when counters are disabled. I don't know the reason
     GPIO_Init(BUTTON0_GPIO_PORT,BUTTON0_GPIO_PIN, GPIO_Mode_In_FL_IT);
-
-    //GPIO_Init(BUTTON1_GPIO_PORT,BUTTON1_GPIO_PIN, GPIO_Mode_In_FL_IT);
+    GPIO_Init(BUTTON1_GPIO_PORT,BUTTON1_GPIO_PIN, GPIO_Mode_In_FL_IT);
 
     EXTI_SetPinSensitivity (EXTI_Pin_7,EXTI_Trigger_Falling);
     EXTI_SetPinSensitivity (EXTI_Pin_6,EXTI_Trigger_Falling);
@@ -24,12 +28,14 @@ int main( void )
     GPIO_Init(LED2_GPIO_PORT,LED2_GPIO_PIN, GPIO_Mode_Out_PP_High_Slow);
     GPIO_Init(LED3_GPIO_PORT,LED3_GPIO_PIN, GPIO_Mode_Out_PP_High_Slow);
 
-    
+    //For some reason PC[5] pin produces interrupt constantly
     GPIOE->CR2 &= ~( (uint8_t)GPIO_Pin_5);
-    GPIOD->CR2 |= ( (uint8_t)GPIO_Pin_6);
-    //GPIOD->CR2 |= ( (uint8_t)GPIO_Pin_7);
-    //GPIOE->CR2 |=  (uint8_t)GPIO_Pin_6;
-    //GPIOC->CR2 |=  (uint8_t)GPIO_Pin_7;
+    // I explored, that interrupt enable bits aren't being set properly.
+    // It happends with buttos, but i will lieave all comands
+    //GPIOD->CR2 |= ( (uint8_t)GPIO_Pin_6); // Interrupt enable of COUNT0
+    //GPIOD->CR2 |= ( (uint8_t)GPIO_Pin_7); // Interrupt enable of COUNT1
+    //GPIOE->CR2 |=  (uint8_t)GPIO_Pin_6; // Interrupt enable of BUT0
+    //GPIOC->CR2 |=  (uint8_t)GPIO_Pin_7; // Interrupt enable of BUT1
     RTC_init();
     enableInterrupts();
     while(1) {
@@ -42,19 +48,19 @@ int main( void )
 }
 
 void RTC_init() {
-    /* Enable RTC clock */
     
+    // Enable external 32.768 KHz oscillator
     CLK_LSEConfig (CLK_LSE_ON);
     while (CLK_GetFlagStatus(CLK_FLAG_LSERDY) == RESET) {}
+    
+    // Divide LSE totally by 64*16=1024 from 32768 Hz to 32 Hz 
     CLK_RTCClockConfig (CLK_RTCCLKSource_LSE, CLK_RTCCLKDiv_64);
     CLK_PeripheralClockConfig(CLK_Peripheral_RTC, ENABLE);
     RTC_WakeUpClockConfig (RTC_WakeUpClock_RTCCLK_Div16);
-
-    /* Configure the RTC */
-    //RTC_WakeUpClockConfig(RTC_WakeUpClock_CK_SPRE_16bits );
     
     RTC_WakeUpCmd(DISABLE); 
-    RTC_SetWakeUpCounter(RTCWUT_FREQ*5);
+    // RTCWUT_FREQ equal to 32 Hz
+    RTC_SetWakeUpCounter( RTCWUT_FREQ*5); //Set up timer for 5 secs
     RTC_ITConfig(RTC_IT_WUT, ENABLE);
     
 }
