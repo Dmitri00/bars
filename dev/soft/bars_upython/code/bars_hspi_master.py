6350ead43779871c772188c8eed1269cc35a6808
Dmitri00 <dima.toichkin@gmail.com>
2019-02-12 14:15:06 +0300

    ESP should wait, while stm is waking up and prepairing for spi communication

--- dev/soft/bars_upython/code/bars_hspi_master.py
+++ dev/soft/bars_upython/code/bars_hspi_master.py
@@ -6,10 +6,20 @@ class HSPIMaster:
     def __init__(self, baud=115200, noc=2):
         self.num_of_counters = noc
         self.hspi = SPI(1, baudrate=baud, polarity=0, phase=0)
-        self.buf = bytearray(1 + 4 * self.num_of_counters)
+        # battery level will be received separetely
+        self.buf = bytearray(4 * self.num_of_counters)
 
     def receive_counters(self):
+        # read spi while stm won't wake up and send byte other, than 0xff
+        stm_halt_mark = 0xff
+        stm_wkup_mark = self.read(1)[0]
+        while stm_wkup_mark == stm_halt_mark:
+            stm_wkup_mark = self.read(1)[0]
+        # first byte, that is being sent by stm is battery level
+        stm_battery_level = stm_wkup_mark
 
+
+        # Read counters from stm
         print("Reading data from SPI...")
 
         self.hspi.readinto(self.buf, 0xff)
