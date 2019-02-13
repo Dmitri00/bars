823bc0dfbbd8ff1ebc3cb6afdd2ffbacbc264b87
2Zhur <mkzhurav@gmail.com>
2019-02-13 12:43:49 +0300

    Did some HSPI master code cleanup, added new debug messages and comments

--- dev/soft/bars_upython/code/bars_hspi_master.py
+++ dev/soft/bars_upython/code/bars_hspi_master.py
@@ -6,21 +6,23 @@ class HSPIMaster:
     def __init__(self, baud=115200, noc=2):
         self.num_of_counters = noc
         self.hspi = SPI(1, baudrate=baud, polarity=0, phase=0)
-        # battery level will be received separetely
         self.buf = bytearray(4 * self.num_of_counters)
 
+        # battery level is received separately
+        self.batt = bytearray(1)
+
     def receive_counters(self):
-        # read spi while stm won't wake up and send byte other, than 0xff
+        print("Waiting for stm to wake up...")
         stm_halt_mark = 0xff
-        stm_wkup_mark = self.read(1)[0]
-        while stm_wkup_mark == stm_halt_mark:
-            stm_wkup_mark = self.read(1)[0]
-        # first byte, that is being sent by stm is battery level
-        stm_battery_level = stm_wkup_mark
+        self.hspi.readinto(self.batt)
 
+        # First byte, not equal to 0xff, is battery level
+        while self.batt[0] == stm_halt_mark:
+            self.hspi.readinto(self.batt)
 
-        # Read counters from stm
-        print("Reading data from SPI...")
+        print("STM is awake!")
+
+        print("Reading data from STM...")
 
         self.hspi.readinto(self.buf, 0xff)
 
@@ -28,10 +30,10 @@ class HSPIMaster:
 
         stm_data = []
         counter_value = 0
-        i = 0
+        i = 1
         j = 0
         while i <= self.num_of_counters:
-            while j < 1 + 4 * i:
+            while j < 4 * i:
                 counter_value <<= 8
                 counter_value |= self.buf[j]
                 j += 1
@@ -39,6 +41,6 @@ class HSPIMaster:
             counter_value = 0
             i += 1
 
-        print("SPI data processed, info received successfully")
+        print("SPI data processed, info received successfully!")
 
         return stm_data
